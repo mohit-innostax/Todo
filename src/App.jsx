892c71c87e14faedbf124cc6127cd0f1fe14268e
Mohit <mohit.kukreja@innostax.com>
2025-02-12 15:22:15 +0530

    Changes to be committed: 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,18 @@ import { FaPen } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 
 function App() {
-  const [status, setStatus] = useState("today");
-  const [toggle,setToggle]=useState('')
-  const [all,setAll]=useState(true);
+  const [status, setStatus] = useState("");
+  const [toggle, setToggle] = useState("");
+  const [all, setAll] = useState(true);
   const [editId, setEditId] = useState(null);
   const [newStatus, setNewStatus] = useState("");
   const [newTaskName, setNewTaskName] = useState("");
   const [formData, setFormData] = useState({
     name: "",
-    cstatus:"",
-    ppriority:"",
+    cstatus: "",
+    ppriority: "",
   });
-  const [error, setError] = useState(false);
+  const [error,setError]=useState(false);
   const arr1 = [
     {
       id: 1,
@@ -64,13 +64,13 @@ function App() {
   ];
   const [arr, setArr] = useState(() => {
     const temp = localStorage.getItem("tasks");
-    return temp ? JSON.parse(temp) : arr1;
+    return temp ? JSON.parse(temp) : [];
   });
 
   useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(arr));
-    console.log(arr);
-  }, [arr]);
+    let date = new Date();
+    console.log(date.getFullYear() + 1);
+  }, []);
 
   const changeStatus = (id) => {
     setEditId(id);
@@ -88,10 +88,12 @@ function App() {
       )
     );
     setEditId(null);
+    localStorage.setItem("tasks", JSON.stringify(arr));
   };
 
   const handleDelete = (id) => {
     setArr(arr.filter((task) => task.id !== id));
+    localStorage.setItem("tasks", JSON.stringify(arr));
   };
 
   const handleError = () => {
@@ -101,68 +103,76 @@ function App() {
       status: formData.cstatus,
       When: formData.ppriority,
     };
-    // if (
-    //   task.length === 0 ||
-    //   cstatus === "Select Status" ||
-    //   ppriority === "Select Priority"
-    // ) {
-    //   setError(true);
-    //   // return;
-    // }
+
     setError(false);
     setArr([...arr, newTask]);
     setFormData({
-      name:"",
-      cstatus:"",
-      ppriority:"",
-    })
+      name: "",
+      cstatus: "",
+      ppriority: "",
+    });
+    localStorage.setItem("tasks", JSON.stringify(arr));
   };
-  const isTaskComplete = formData.name && formData.cstatus && formData.ppriority;
+  const isTaskComplete =
+    formData.name && formData.cstatus && formData.ppriority;
+
+  const handleChange = (e) => {
+    e.target.value === "all" ? setAll(true) : setAll(false);
+    console.log(e.target.value);
+    setStatus(e.target.value);
+  };
 
   return (
     <>
-      <div className="flex bg-black text-white h-10 mt-4 justify-center items-center p-10 text-4xl">
+      <div className="flex bg-black text-white h-16 mt-4 justify-center items-center top-0 sticky text-4xl">
         TODO App
       </div>
-      <span className="flex justify-center align-center mt-12 gap-2">
-        <button
-          className={`bg-gray-100 p-4 rounded-md hover:bg-green-200 ease-in-out 2s ${toggle==='today'? 'bg-green-500':''} `}
-          onClick={() => {
-            setToggle('today');
-            setAll(false);
-            setStatus("today");
-          }}
+      <div className="flex justify-center items-center p-6">
+      <select
+        className="flex justify-center  mt-6 gap-4 border-5 p-3 rounded-md"
+        value={status}
+        onChange={handleChange}
+      >
+        <option value="" disabled selected>
+          Select Time
+        </option>
+        <option value="all">All</option>
+        <option
+          className={`bg-gray-100 p-4 rounded-md hover:bg-green-200 ease-in-out 2s ${
+            toggle === "today" ? "bg-green-500" : ""
+          } `}
+          value="today"
         >
           Today
-        </button>
-        <button
-          className={`bg-gray-100 p-4 rounded-md hover:bg-green-200 ease-in-out 2s ${toggle==='any day'? 'bg-green-500':''} `}
-          onClick={() => {
-            setToggle('any day');
-            setAll(false);
-            setStatus("any day");
-          }}
+        </option>
+        <option
+          className={`bg-gray-100 p-4 rounded-md hover:bg-green-200 ease-in-out 2s ${
+            toggle === "any day" ? "bg-green-500" : ""
+          } `}
+          value="any day"
         >
           Any day
-        </button>
-      </span>
-      <div className="bg-gray-200 mt-5 px-3 gap-11 p-5 ">
-        <h1 className="font-bold text-lg h-full w-full relative mb-10">
-          Tasks
-        </h1>
-        <div className="flex gap-3 mb-3 ">
+        </option>
+      </select>
+      </div>
+      <div className="bg-gray-200 mt-5 mx-4 gap-11 p-5 ">
+        <div className="flex gap-4 mb-4 ">
           <input
-            className="border-2 p-2 rounded-md  "
+            className="border-2 p-2 rounded w-1/3  "
             value={formData.name}
             placeholder="Enter Task Name"
-            onChange={(e) => setFormData({...formData,name:e.target.value})}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
           />
           <select
             value={formData.cstatus}
-            onChange={(e) => setFormData({...formData,cstatus:e.target.value})}
-            className=" border-2 p-2 rounded-md"
+            onChange={(e) =>
+              setFormData({ ...formData, cstatus: e.target.value })
+            }
+            className=" border-2 p-2 rounded w-1/4"
           >
-            <option value="" disabled selected>Select Status</option>
+            <option value="" disabled selected>
+              Select Status
+            </option>
             <option value="Ready for Deployment">Ready for Deployment</option>
             <option value="In Progress">In Progress</option>
             <option value="Development Review">Development Review</option>
@@ -170,84 +180,101 @@ function App() {
           </select>
           <select
             value={formData.ppriority}
-            onChange={(e) => setFormData({...formData,ppriority:e.target.value})}
+            onChange={(e) =>
+              setFormData({ ...formData, ppriority: e.target.value })
+            }
             className="border-2 p-2 rounded-md"
           >
-            <option value="" disabled selected>Select Priority</option>
+            <option value="" disabled selected>
+              Select Priority
+            </option>
             <option value="Today">Today</option>
             <option value="Any Day">Any Day</option>
           </select>
           <button
             disabled={!isTaskComplete}
-            className="border-2 p-3 rounded-md"
+            className={` bg-green-500 text-white px-4 py-2 rounded-md ${!isTaskComplete? "cursor-not-allowed":"cursor-pointer"}`}
             onClick={() => {
               handleError();
             }}
           >
             Add
           </button>
-          {/* {
-            true?(<h1>Please Check the details you entered</h1>):(<h1></h1>)
-          } */}
         </div>
-        <table>
-          <tr>
-            <th>Task name</th>
-            <th>Task Status</th>
+        <table className="w-full border-collapse">
+          <tr className="bg-gray-300">
+            <th className="border p-2">Task name</th>
+            <th className="border p-2">Status</th>
+            <th className="border p-2">Priority</th>
+            <th className="border p-2">Action</th>
           </tr>
+          <tbody>
+            {arr
+              .filter((item) => {
+                return all || item.When.toLowerCase() === status;
+              })
+              .map((item) => (
+                <tr key={item.id} className="text-center w-full h-full">
+                  {editId === item.id ? (
+                    <>
+                      <td className="border p-2">
+                        <input
+                          className="border p-1 w-full rounded-md"
+                          value={newTaskName}
+                          onChange={(e) => setNewTaskName(e.target.value)}
+                        />
+                      </td>
+                      <td className="border p-2">
+                        <select
+                          value={newStatus}
+                          onChange={(e) => setNewStatus(e.target.value)}
+                          className=" border p-1 rounded-md w-full "
+                        >
+                          <option value="In Progress">In Progress</option>
+                          <option value="Development Review">
+                            Development Review
+                          </option>
+                          <option value="Completed">Completed</option>
+                        </select>
+                      </td>
+                      <td className="border p-2">{item.When}</td>
+                      <td className="border p-2">
+                        <button
+                          className="px-3 py-1 bg-blue-300 rounded cursor-pointer"
+                          onClick={() => saveStatus(item.id)}
+                        >
+                          Save
+                        </button>
+                      </td>
+                    </>
+                  ) : (
+                    <>
+                      <td key={item.id} className="border p-2">
+                        {item.task}
+                      </td>
+                      <td className="border p-2">{item.status}</td>
+                      <td className="border p-2">{item.When}</td>
+                      <td className="border-3 p-3.5 flex justify-center gap-2 relative max-h-full">
+                        <FaPen
+                          className="text-blue-500 cursor-pointer"
+                          onClick={() => {
+                            changeStatus(item.id);
+                          }}
+                        />
+                        <MdDelete
+                          className="text-red-500 cursor-pointer"
+                          onClick={() => {
+                            handleDelete(item.id);
+                          }}
+                        />
+                      </td>
+                    </>
+                  )}
+                </tr>
+              ))}
+          </tbody>
         </table>
-        <div className="grid gap-5">
-          {arr
-            .filter((item) => {
-              return all || item.When.toLowerCase() === status;
-            })
-            .map((item) => (
-              <div className="flex justify-evenly items-start border-2 border-indigo-600 px-3 py-3 rounded-md hover:bg-gray-300">
-                {editId === item.id ? (
-                  <>
-                    <input
-                      className="border p-1 rounded-md"
-                      value={newTaskName}
-                      onChange={(e) => setNewTaskName(e.target.value)}
-                    />
-                    <select
-                      value={newStatus}
-                      onChange={(e) => setNewStatus(e.target.value)}
-                      className=" border p-1 rounded-md "
-                    >
-                      <option value="In Progress">In Progress</option>
-                      <option value="Development Review">
-                        Development Review
-                      </option>
-                      <option value="Completed">Completed</option>
-                    </select>
-                    <button
-                      className="ml-2 p-1 bg-blue-300 rounded "
-                      onClick={() => saveStatus(item.id)}
-                    >
-                      Save
-                    </button>
-                  </>
-                ) : (
-                  <>
-                    <h2 key={item.id}>{item.task}</h2>
-                    <span>{item.status}</span>
-                    <span>{item.When}</span>
-                  </>
-                )}
-                <FaPen
-                  onClick={() => {
-                    changeStatus(item.id);
-                  }}
-                />
-                <MdDelete
-                  onClick={() => {
-                    handleDelete(item.id);
-                  }}
-                />
-              </div>
-            ))}
-        </div>
+        {arr.length === 0 ? <div className="text-red-600 text-xl py-5">Their is no task present currently, please add some new task</div>: <div></div> }
       </div>
     </>
   );
